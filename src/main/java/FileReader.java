dad67d930860c0ebd95409fe90f3bd00b6687157
Rashid <seerroman@mail.ru>
2020-07-10 23:53:45 +0300

    Bad way to read

--- src/main/java/FileReader.java
+++ src/main/java/FileReader.java
@@ -1,9 +1,42 @@
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileReader {
+    private static String listOfColleagues = "list.txt";
 
     public static void main(String[] args) {
-        //TODO
+        InputStream dataStream = FileReader.class.getResourceAsStream(listOfColleagues);
+        BufferedReader br = null;
+        StringBuffer sb = new StringBuffer();
+        String s;
+        List<String> arrayListOfColleagues = new ArrayList<String>();
+        try {
+            br = new BufferedReader(new InputStreamReader(dataStream));
+            int i = 0;
+            while((s = br.readLine())!=null){
+                arrayListOfColleagues.add(i,s);
+                i++;
+            }
+
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(br == null){
+                try{ br.close();}
+                catch (IOException ex){
+                    ex.printStackTrace();
+                }
+            }
+        }
+        System.out.println("+++" + arrayListOfColleagues);
 
     }
 }
