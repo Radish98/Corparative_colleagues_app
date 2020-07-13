4edb70c7203a2923b0d9070b5b4804bb748c9cb6
Rashid <seerroman@mail.ru>
2020-07-13 22:21:29 +0300

    Right way to read

--- src/main/java/FileReader.java
+++ src/main/java/FileReader.java
@@ -1,42 +1,63 @@
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class FileReader {
-    private static String listOfColleagues = "list.txt";
+//    private static String listOfColleagues = "list.txt";
+    private static String absolutePath = "src/main/resources/list.txt";
+    private static File justFile = new File (absolutePath);
+    private static List<String> listOfColleagues;
+//    private static List<String> justList;
+
+
+
 
     public static void main(String[] args) {
-        InputStream dataStream = FileReader.class.getResourceAsStream(listOfColleagues);
-        BufferedReader br = null;
-        StringBuffer sb = new StringBuffer();
-        String s;
-        List<String> arrayListOfColleagues = new ArrayList<String>();
-        try {
-            br = new BufferedReader(new InputStreamReader(dataStream));
-            int i = 0;
-            while((s = br.readLine())!=null){
-                arrayListOfColleagues.add(i,s);
-                i++;
-            }
+//        InputStream dataStream = FileReader.class.getResourceAsStream(listOfColleagues);
+//        BufferedReader br = null;
+//        StringBuffer sb = new StringBuffer();
+//        String s;
+//        List<String> arrayListOfColleagues = new ArrayList<>();
+//        try {
+//            br = new BufferedReader(new InputStreamReader(dataStream));
+//            int i = 0;
+//            while((s = br.readLine())!=null){
+//                arrayListOfColleagues.add(i,s);
+//                i++;
+//            }
+//
+//        } catch (NullPointerException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            if(br == null){
+//                try{ br.close();}
+//                catch (IOException ex){
+//                    ex.printStackTrace();
+//                }
+//            }
+//        }
+//        System.out.println("+++" + arrayListOfColleagues);
 
-        } catch (NullPointerException e) {
-            e.printStackTrace();
+        try {
+            System.out.println("++++++++++++++");
+            listOfColleagues = Files.readAllLines(Paths.get(justFile.toURI()));
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            if(br == null){
-                try{ br.close();}
-                catch (IOException ex){
-                    ex.printStackTrace();
-                }
-            }
         }
-        System.out.println("+++" + arrayListOfColleagues);
+        System.out.println("All list:" + listOfColleagues);
+        
+        for(int i = 0; i < listOfColleagues.size(); i++){
+            String fio = listOfColleagues.get(i).split("/")[0];
+            String department = listOfColleagues.get(i).split("/")[1];
+            String salary = listOfColleagues.get(i).split("/")[2];
+
+        }
+
 
     }
 }
